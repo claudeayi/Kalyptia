c25f915928ec0af4d64314fde9d1f1e5e65b42e8
claudeayi <claudejordanayissi@gmail.com>
2025-08-28 22:19:57 +0100

    Update main.jsx

--- Front-end /src/main.jsx
+++ Front-end /src/main.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles/index.css";
+import "./index.css";
+import { ThemeProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
 );
