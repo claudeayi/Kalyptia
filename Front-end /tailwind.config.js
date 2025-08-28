8205afdc929e35da4479b39b1a9cbf003800b68d
claudeayi <claudejordanayissi@gmail.com>
2025-08-28 22:27:27 +0100

    Update tailwind.config.js

--- Front-end /tailwind.config.js
+++ Front-end /tailwind.config.js
@@ -1,7 +1,8 @@
 export default {
+  darkMode: "class", // ✅ Ajout pour activer le mode sombre via la classe "dark"
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
