cb82e76debf75daaba1fabcee92d099cc0938cc8
claudeayi <claudejordanayissi@gmail.com>
2025-08-29 00:22:00 +0100

    Create src/context/NotificationContext.jsx

---
+++ Front-end /src/context/NotificationContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+const NotificationContext = createContext();
+
+export const NotificationProvider = ({ children }) => {
+  const [notifications, setNotifications] = useState([]);
+
+  const addNotification = (notif) => {
+    setNotifications((prev) => [notif, ...prev]);
+  };
+
+  const removeNotification = (id) => {
+    setNotifications((prev) => prev.filter((n) => n.id !== id));
+  };
+
+  return (
+    <NotificationContext.Provider
+      value={{ notifications, addNotification, removeNotification }}
+    >
+      {children}
+    </NotificationContext.Provider>
+  );
+};
+
+export const useNotifications = () => useContext(NotificationContext);
