f6d8d3314bf989303663db9bcbc756b1842de7a8
claudeayi <claudejordanayissi@gmail.com>
2025-08-28 19:04:28 +0100

    Update Dashboard.jsx

--- Front-end /src/pages/Dashboard.jsx
+++ Front-end /src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import Notification from "../components/Notification"; // ✅ ajout
 
 export default function Dashboard() {
   return (
@@ -15,6 +16,9 @@ export default function Dashboard() {
           <Outlet />
         </main>
       </div>
+
+      {/* Notifications temps réel */}
+      <Notification /> {/* ✅ ajout */}
     </div>
   );
 }
